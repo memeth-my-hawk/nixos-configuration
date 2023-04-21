08785fef12830425fd278844b57aba9b76c65457
Mehmet Karaman <59482075+memeth-my-hawk@users.noreply.github.com>
2023-04-21 23:22:14 +0300

    Update networking.nix

--- modules/networking.nix
+++ modules/networking.nix
@@ -20,11 +20,11 @@
       networks = {
 
         memeth-0 = {
-          pskRaw = "4fa655e68f36d2e7459f08c342ae28586ca095b512471fb94afc608934c981a0";
+          pskRaw = "";
         };
 
         cclub = {
-          pskRaw = "d961dfdd98ca88620f71b826e0bf8d92d7fa118ed75b5e65bbfb5f6ed967155c";
+          pskRaw = "";
         };
 
         meturoam = {
