c5e3949ecc36c6203d798737d2f27145c5e05869
Mehmet Karaman <59482075+memeth-my-hawk@users.noreply.github.com>
2023-07-21 16:27:13 +0300

    Add files via upload

---
+++ modules/aile.nix
@@ -0,0 +1,159 @@
+{ config, pkgs, lib, ... }:
+
+{
+   # Hardware settings.
+  hardware = {
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = false;
+    bluetooth.settings = {
+      General = {
+        Experimental = true;
+        KernelExperimental = true;
+      };
+    };
+
+    cpu.intel.updateMicrocode = true;
+    nvidia.modesetting.enable = true;
+
+
+
+
+  };
+
+  services.printing.drivers = [ pkgs.hplipWithPlugin pkgs.samsung-unified-linux-driver ];
+  boot.blacklistedKernelModules = ["nouveau"];
+
+
+
+  # Networking settings
+  networking = {
+    hostName = ""; # Define your hostname.
+    networkmanager.enable = true;
+  };
+
+  #programs.nm-applet.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+
+  # Enable KDE Plasma
+  services.xserver.enable = true;
+  services.xserver.layout = "tr";
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    plasma-pa
+    bluedevil
+    ark
+    polkit-kde-agent
+  ];
+
+  programs.partition-manager.enable = true;
+  #services.power-profiles-daemon.enable = false;
+
+  # Services and daemons.
+  services = {
+
+    # Enable Pipewire service.
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    # Enable Printing service.
+    printing = {
+      enable = true;
+    };
+
+    # Enable TRIM support for SSDs
+    fstrim.enable = true;
+
+    # Enable Syncthing service
+    #syncthing = {
+    #  enable = true;
+    #  user = "";
+    #  dataDir = "";
+    #};
+
+    # Enable mlocate service
+    locate = {
+      enable = true;
+      locate = pkgs.mlocate;
+      interval = "hourly";
+      localuser = null;
+    };
+
+  
+  };
+
+  # Nix garbage collection service.
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+  };
+
+
+
+
+  fonts.fonts = with pkgs; [
+    source-code-pro
+    cantarell-fonts
+    corefonts
+    carlito
+    caladea
+  ];
+
+
+  # Packages and package settings
+  nixpkgs.config.allowUnfree = true;
+  #services.flatpak.enable = true;
+  environment.systemPackages = with pkgs; [
+      wget
+      neofetch
+      firefox
+      htop
+      zoom-us
+      gcc
+      gnumake
+      libreoffice-fresh
+      microcodeIntel
+      linuxHeaders
+      git
+      mlocate
+      lm_sensors
+      smartmontools
+      ventoy-full
+      dmidecode
+      ntfs3g
+      acpi
+      parted
+      android-file-transfer
+      droidcam
+      usbutils
+      zip
+      unzip
+      shared-mime-info
+      mimeo
+      #networkmanagerapplet
+      tree
+      rar
+      google-chrome
+      vlc
+  ];
+
+    
+
+
+
+
+}
